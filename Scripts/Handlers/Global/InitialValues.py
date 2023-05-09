2ef84da539c90f60c36c6af326cf0e1be81e3ec7
Hamidreza Zamanian <hamid80zamanian@gmail.com>
2023-05-09 14:36:00 +0330

    handle wrong lang code from Tel user's settings

--- Scripts/Handlers/Global/InitialValues.py
+++ Scripts/Handlers/Global/InitialValues.py
@@ -17,9 +17,15 @@ def user_data_init(user_id: str, message: Message = None):
     dbh = UserDataHandler()
     if message and not dbh.user_exists(user_id=user_id):
         language_code = message.from_user.language_code
-        if Translator.validate_language(language=language_code):
-            dbh.create_user(user_id=user_id, user_plan=None,
-                            user_language=language_code, user_artists=[])
+
+        # handle no/wrong language code
+        try:
+            Translator.validate_language(language=language_code)
+        except:
+            language_code = 'en'
+
+        dbh.create_user(user_id=user_id, user_plan=None,
+                        user_language=language_code, user_artists=[])
     (tr := Translator()).set_language(
         dbh.get_language(user_id=user_id))
     dbh.save_database()
